52ea9d2ceaa7a305764ffe625270339037b900ac
unknown <azad_ug@cse.nits.ac.in>
2021-03-23 01:36:49 +0530

    Basic Array Generation DOne

---
+++ Js/main.js
@@ -0,0 +1,32 @@
+var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
+var inp_gen=document.getElementById("a_generate");
+var inp_aspeed=document.getElementById("a_speed");
+var butts_algos=document.querySelectorAll(".algo button");
+var div_sizes=[];
+var divs=[];
+var margin_size;
+var cont=document.getElementById("array_container");
+cont.style="flex-direction:row";
+inp_gen.addEventListener("click",generate_array);
+inp_as.addEventListener("input",update_array_size);
+function generate_array()
+{
+    cont.innerHTML="";
+
+    for(var i=0;i<array_size;i++)
+    {
+        div_sizes[i]=Math.floor(Math.random() * 0.5*(inp_as.max - inp_as.min) ) + 10;
+        divs[i]=document.createElement("div");
+        cont.appendChild(divs[i]);
+        margin_size=0.1;
+        divs[i].style=" margin:0% " + margin_size + "%; background-color:#E5C2C1; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+    }
+}
+
+function update_array_size()
+{
+    array_size=inp_as.value;
+    generate_array();
+}
+
+window.onload=update_array_size();
